23b3c02b2d2015dfbbc0e1bee028f521ca4a92b1
Ori Nahum <orinah@rafael.co.il>
2023-01-02 17:38:36 +0200

    text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./Text.css";
 
 export type TextSize = "small" | "medium" | "large";
@@ -5,10 +6,24 @@ export type TextSize = "small" | "medium" | "large";
 interface props {
   textValue: string;
   textSize?: TextSize;
+  textColor?: string;
 }
 
-const Text = ({ textValue, textSize = "medium" }: props) => {
-  return <span className={`text-text ${textSize}`}>{textValue}</span>;
+const Text = ({
+  textColor = "black",
+  textValue,
+  textSize = "medium",
+}: props) => {
+  
+  const style: React.CSSProperties = {
+    color: textColor,
+  };
+
+  return (
+    <span style={style} className={`text-text ${textSize}`}>
+      {textValue}
+    </span>
+  );
 };
 
 export { Text };
