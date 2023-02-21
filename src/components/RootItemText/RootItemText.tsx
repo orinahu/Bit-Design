1c1a9053d10de8f02f03f4e7b38658732e19645a
“Ori <ori.nahu@gmail.com>
2023-02-21 11:49:37 +0200

    add selceted option to rootListItem

--- src/components/RootItemText/RootItemText.tsx
+++ src/components/RootItemText/RootItemText.tsx
@@ -1,42 +1,46 @@
-import './RootItemText.css'
+import "./RootItemText.css";
 import { Icon, IconSize, IconForm } from "../../in";
 import { Text, Size } from "../../in";
 
 interface props {
-    name: string;
-    number: number;
-    iconName?: string;
-    iconColor?: string;
-    iconSize?: IconSize;
-    iconForm?: IconForm;
-    textSize?: Size;
-    className?: string;
-    style?: React.CSSProperties;
+  name: string;
+  number: number;
+  iconName?: string;
+  iconColor?: string;
+  iconSize?: IconSize;
+  iconForm?: IconForm;
+  textSize?: Size;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 const RootItemText = ({
-    iconColor,
-    name,
-    number,
-    iconSize = "lg",
-    iconForm,
-    iconName = "image",
-    textSize = "medium",
-    className,
-    style
+  iconColor,
+  name,
+  number,
+  iconSize = "lg",
+  iconForm,
+  iconName = "image",
+  textSize = "medium",
+  className,
+  style,
 }: props) => {
-    return (
-        <div className={`bit-list-item-root-name ${className}`} style={style}>
-            <Icon
-                iconName={iconName}
-                iconForm={iconForm}
-                iconSize={iconSize}
-                iconColor={iconColor}
-            />
-            <Text size={textSize} text={`${name[0].toUpperCase() + name.slice(1)} (${number.toString()} components)`} />
-        </div>
+  return (
+    <div className={`bit-list-item-root-name ${className}`} style={style}>
+      <Icon
+        iconName={iconName}
+        iconForm={iconForm}
+        iconSize={iconSize}
+        iconColor={iconColor}
+      />
+      <Text
+        size={textSize}
+        text={`${
+          name[0].toUpperCase() + name.slice(1)
+        } (${number.toString()} components)`}
+      />
+    </div>
+  );
+};
 
-    )
-}
-
-export { RootItemText }
+export { RootItemText };
--- src/components/RootListItem/RootListItem.tsx
+++ src/components/RootListItem/RootListItem.tsx
@@ -1,66 +1,77 @@
-import './RootListItem.css'
+import "./RootListItem.css";
 import { RootItemText, TrafficLights } from "../../in";
 import { Text, Size, IconSize, IconForm, componenetsStatus } from "../../in";
 
-
 interface statusComponents {
-    ComponentStatus: componenetsStatus;
-    counter: number;
+  ComponentStatus: componenetsStatus;
+  counter: number;
 }
 
 interface props {
-    textName: string;
-    textNumber: number;
-    textIconName?: string;
-    textIconColor?: string;
-    textIconSize?: IconSize;
-    textIconForm?: IconForm;
-    textTextSize?: Size;
-    textClassName?: string;
-    textStyle?: React.CSSProperties;
+  textName: string;
+  textNumber: number;
+  textIconName?: string;
+  textIconColor?: string;
+  textIconSize?: IconSize;
+  textIconForm?: IconForm;
+  textTextSize?: Size;
+  textClassName?: string;
+  textStyle?: React.CSSProperties;
 
-    SDerivedStatusCounter: statusComponents[]
-    TraficLightsTextSize?: Size;
-    TraficLightsClassName?: string;
+  SDerivedStatusCounter: statusComponents[];
+  TraficLightsTextSize?: Size;
+  TraficLightsClassName?: string;
 
+  selected?: boolean;
+
+  className?: string;
 }
 
 const RootListItem = ({
-    textName,
-    textNumber,
-    textIconName,
-    textIconColor,
-    textIconSize,
-    textIconForm,
-    textTextSize,
-    textClassName,
-    textStyle,
+  textName,
+  textNumber,
+  textIconName,
+  textIconColor,
+  textIconSize,
+  textIconForm,
+  textTextSize,
+  textClassName,
+  textStyle,
 
-    SDerivedStatusCounter,
-    TraficLightsTextSize,
-    TraficLightsClassName
+  SDerivedStatusCounter,
+  TraficLightsTextSize,
+  TraficLightsClassName,
 
+  selected = false,
+  className,
 }: props) => {
-    return (
-        <div className='bit-root-list-item'>
-            <RootItemText
-                name={textName}
-                number={textNumber}
-                iconName={textIconName}
-                iconColor={textIconColor}
-                iconSize={textIconSize}
-                iconForm={textIconForm}
-                textSize={textTextSize}
-                className={textClassName}
-                style={textStyle}
-            />
-            <div className='bit-root-list-item-divider'></div>
-            <div>
-                <TrafficLights className={TraficLightsClassName} textSize={TraficLightsTextSize} SDerivedStatusCounter={SDerivedStatusCounter} />
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className={`bit-root-list-item-container ${className} ${
+        selected && "selected"
+      }`}
+    >
+      <RootItemText
+        name={textName}
+        number={textNumber}
+        iconName={textIconName}
+        iconColor={textIconColor}
+        iconSize={textIconSize}
+        iconForm={textIconForm}
+        textSize={textTextSize}
+        className={textClassName}
+        style={textStyle}
+      />
+      <div className="bit-root-list-item-divider"></div>
+      <div>
+        <TrafficLights
+          className={TraficLightsClassName}
+          textSize={TraficLightsTextSize}
+          SDerivedStatusCounter={SDerivedStatusCounter}
+        />
+      </div>
+    </div>
+  );
+};
 
-}
-
-export { RootListItem }
+export { RootListItem };
